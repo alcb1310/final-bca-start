574be1251d55b20a5823012def047bcd3a5109bd
Andres Court <andres@andrescourt.com>
2025-12-02 21:08:52 -0500

    chore(tanstack-start): deployment configuratoin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import viteReact from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
     plugins: [
         tsConfigPaths(),
         tanstackStart(),
+        nitro(),
         // react's vite plugin must come after start's vite plugins
         viteReact(),
     ]
