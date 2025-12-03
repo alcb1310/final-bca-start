d3a66f84287f6679d5d6bdab46b2a86bbc545ac6
Andres Court <andres@andrescourt.com>
2025-12-02 20:46:54 -0500

    chore(tanstack-start): create application's root

---
+++ src/routes/__root.tsx
@@ -0,0 +1,33 @@
+import { ReactNode } from "react"
+import { createRootRoute, HeadContent, Outlet, Scripts } from "@tanstack/react-router";
+
+export const Route = createRootRoute({
+    head: () => ({
+        meta: [
+            { charset: "utf-8" },
+            { name: "viewport", content: "width=device-width, initial-scale=1" },
+            { title: "Budget Control Application" },
+        ],
+    }),
+    component: RootComponent,
+})
+
+function RootComponent() {
+    return (
+        <RootDocument>
+            <Outlet />
+        </RootDocument>
+    )
+}
+
+function RootDocument({ children }: { children: ReactNode }) {
+    return (
+        <html lang="en">
+            <head> <HeadContent /> </head>
+            <body>
+                {children}
+                <Scripts />
+            </body>
+        </html>
+    )
+}
