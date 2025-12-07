98ee552acd1297a5cef55b04a927edc45e86160f
Andres Court <andres@andrescourt.com>
2025-12-07 13:05:39 -0500

    chore(better-auth): tanstack start usage tips

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,18 +1,22 @@
 import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { createAuthClient } from 'better-auth/client'
+import { tanstackStartCookies } from 'better-auth/tanstack-start'
 import { drizzle } from 'drizzle-orm/node-postgres'
+import * as schema from '@/db/schema'
 
 const db = drizzle(process.env.DATABASE_URL || '')
 
 export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: 'pg',
+        schema,
     }),
     baseURL: process.env.BETTER_AUTH_URL || '',
     emailAndPassword: {
         enabled: true,
     },
+    plugins: [tanstackStartCookies()],
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
