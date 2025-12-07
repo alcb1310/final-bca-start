a9bc6bb476c687a07f415c30ec3b2ab75bff3456
Andres Court <andres@andrescourt.com>
2025-12-07 17:49:05 -0500

    feat(tanstack-form): create submit button component

---
+++ src/components/form/Button.tsx
@@ -0,0 +1,19 @@
+import type { ComponentProps } from 'react'
+import { useFormContext } from '@/hooks/formHook'
+import { Button } from '../ui/button'
+
+interface FormButtonProps extends ComponentProps<'button'> {
+    label: string
+}
+
+export function FormButton({ label, ...props }: FormButtonProps) {
+    const form = useFormContext()
+
+    return (
+        <form.Subscribe selector={(state) => state.isSubmitting}>
+            <Button variant='default' {...props}>
+                {label}
+            </Button>
+        </form.Subscribe>
+    )
+}
