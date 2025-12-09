878d5e786056a7cb7adc5e6d4fcb688135b77dac
Andres Court <andres@andrescourt.com>
2025-12-09 07:03:37 -0500

    feat(sidebar): sidebar structure

--- src/components/nav/SideBar.tsx
+++ src/components/nav/SideBar.tsx
@@ -1,3 +1,85 @@
+import { Link } from '@tanstack/react-router'
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarSeparator,
+} from '@/components/ui/sidebar'
+import type { FileRoutesByTo } from '@/routeTree.gen'
+import {
+    type LucideProps,
+    ShoppingCartIcon,
+    CreditCardIcon,
+    FolderOpenIcon,
+} from 'lucide-react'
+import type { ForwardRefExoticComponent, RefAttributes } from 'react'
+
+type MenuItem = {
+    title: string
+    // TODO: remove the '#' option
+    path: keyof FileRoutesByTo | '#'
+    icon?: ForwardRefExoticComponent<
+        Omit<LucideProps, 'ref'> & RefAttributes<SVGSVGElement>
+    >
+}
+
+type Menu = {
+    title: string
+    items: MenuItem[]
+}
+
+const menu: Menu[] = [
+    {
+        title: 'Transacciones',
+        items: [
+            { title: 'Presupuesto', path: '#', icon: ShoppingCartIcon },
+            { title: 'Facturas', path: '#', icon: CreditCardIcon },
+        ],
+    },
+    {
+        title: 'Reportes',
+        items: [{ title: 'Actual', path: '#', icon: FolderOpenIcon }],
+    },
+]
+
 export default function SideBar() {
-    return <div>Side Bar</div>
+    return (
+        <Sidebar>
+            <SidebarHeader className='flex justify-center items-center text-2xl font-bold'>
+                <Link to='/dashboard'>BCA</Link>
+            </SidebarHeader>
+            <SidebarSeparator />
+            <SidebarContent>
+                {menu.map((men) => (
+                    <SidebarGroup key={men.title}>
+                        <SidebarGroupLabel>{men.title}</SidebarGroupLabel>
+                        <SidebarGroupContent>
+                            <SidebarMenu>
+                                {men.items.map((item) => (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild>
+                                            <Link to={item.path}>
+                                                <div className='flex gap-4 justify-center'>
+                                                    {item.icon && (
+                                                        <item.icon size={16} />
+                                                    )}
+                                                    <span>{item.title}</span>
+                                                </div>
+                                            </Link>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                ))}
+                            </SidebarMenu>
+                        </SidebarGroupContent>
+                    </SidebarGroup>
+                ))}
+            </SidebarContent>
+        </Sidebar>
+    )
 }
