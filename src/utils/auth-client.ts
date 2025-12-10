0bb239043a569198ad85126307a5ddb2329b5996
Andres Court <andres@andrescourt.com>
2025-12-10 18:54:04 -0500

    feat(better-auth): enable client plugin

--- src/utils/auth-client.ts
+++ src/utils/auth-client.ts
@@ -1,6 +1,8 @@
 import { createAuthClient } from 'better-auth/client'
+import { adminClient } from 'better-auth/client/plugins'
 
 export const authClient = createAuthClient({
     baseURL: process.env.BETTER_AUTH_URL || '',
     redirectTo: '/login',
+    plugins: [adminClient()],
 })
