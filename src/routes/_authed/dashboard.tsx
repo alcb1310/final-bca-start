75c3a2785906eeeaf8cc00350c5297701086be28
Andres Court <andres@andrescourt.com>
2025-12-09 19:02:34 -0500

    feat(dashboard): display personalized welcome message

--- src/routes/_authed/dashboard.tsx
+++ src/routes/_authed/dashboard.tsx
@@ -2,8 +2,22 @@ import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authed/dashboard')({
     component: RouteComponent,
+    loader: async ({ serverContext }) => {
+        return {
+            user: serverContext?.user,
+        }
+    },
 })
 
 function RouteComponent() {
-    return <div>Hello "/_authed/dashboard"!</div>
+    const { user } = Route.useLoaderData()
+
+    return (
+        <div>
+            Bienvenido&nbsp;
+            <span className='py-1 px-2 bg-accent text-accent-foreground'>
+                {user?.name}
+            </span>
+        </div>
+    )
 }
