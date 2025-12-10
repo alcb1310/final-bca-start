5a7c067c935480badf8c391cfe6d6efc1e50864b
Andres Court <andres@andrescourt.com>
2025-12-09 21:56:29 -0500

    feat(topbar): logout from the app and go to login page

--- src/components/nav/TopBar.tsx
+++ src/components/nav/TopBar.tsx
@@ -1,17 +1,40 @@
+import { authClient } from '@/utils/auth-client'
+import { useNavigate } from '@tanstack/react-router'
 import { LogOutIcon, UserIcon } from 'lucide-react'
+import { toast } from 'sonner'
 import { Button } from '../ui/button'
 
 export default function TopBar() {
+    const navigate = useNavigate()
+    async function handleLogout() {
+        await authClient.signOut(
+            {},
+            {
+                onSuccess: () => {
+                    navigate({ to: '/login' })
+                },
+                onError: (error) => {
+                    toast.error('Error al cerrar sesión', {
+                        richColors: true,
+                        position: 'top-center',
+                    })
+
+                    console.error(error)
+                },
+            },
+        )
+    }
+
     return (
         <nav className='flex justify-between items-center p-4'>
-            <h2 className='text-text-xl font-bold'>
+            <h3 className='text-xl font-bold'>
                 Sistema Control Presupuestario
-            </h2>
+            </h3>
             <div>
                 <Button variant='ghost' size='icon'>
                     <UserIcon />
                 </Button>
-                <Button variant='ghost' size='icon'>
+                <Button variant='ghost' size='icon' onClick={handleLogout}>
                     <LogOutIcon />
                 </Button>
             </div>
