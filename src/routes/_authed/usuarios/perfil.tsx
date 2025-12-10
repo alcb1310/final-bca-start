c64ea49600b3340f7a4262908579633b3edba55e
Andres Court <andres@andrescourt.com>
2025-12-10 00:03:27 -0500

    feat(profile): display user profile information

--- src/routes/_authed/usuarios/perfil.tsx
+++ src/routes/_authed/usuarios/perfil.tsx
@@ -1,9 +1,39 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
+import { PageTitle } from '@/components/pages/Title'
+import { authClient } from '@/utils/auth-client'
 
 export const Route = createFileRoute('/_authed/usuarios/perfil')({
     component: RouteComponent,
+    loader: async () => {
+        const { data: session } = await authClient.getSession()
+
+        return {
+            user: session?.user,
+        }
+    },
 })
 
 function RouteComponent() {
-    return <div>Hello "/_authed/usuarios/perfil"!</div>
+    const { user } = Route.useLoaderData()
+
+    return (
+        <>
+            <PageTitle title='Perfil' />
+            <div>
+                <p>
+                    <span className='text-green-600'>Nombre: </span>
+                    {user?.name}
+                </p>
+                <p>
+                    <span className='text-green-600'>Email: </span>
+                    {user?.email}
+                </p>
+                <br />
+                <p className='text-xs'>
+                    <span className='font-bold'>Nota:</span> Para modificar el
+                    perfil, favor contactarse con el administrador.
+                </p>
+            </div>
+        </>
+    )
 }
