3548d6fbdc27bf1297004ad8331186bebd2ba13c
Andres Court <andres@andrescourt.com>
2025-12-14 18:59:59 -0500

    feat(projects): Create project form

--- src/components/drawers/parametros/proyectos/create-project.tsx
+++ src/components/drawers/parametros/proyectos/create-project.tsx
@@ -1,11 +1,114 @@
 import { PlusIcon } from 'lucide-react'
+import z from 'zod'
 import { Button } from '@/components/ui/button'
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from '@/components/ui/drawer'
+import { Field, FieldGroup, FieldSet } from '@/components/ui/field'
+import { useAppForm } from '@/hooks/formHook'
+
+export const projectCreateSchema = z.object({
+    name: z
+        .string({ message: 'El nombre es requerido' })
+        .min(1, { message: 'El nombre es requerido' }),
+    is_active: z.boolean(),
+    gross_area: z.coerce.number({ message: 'Ingrese un número válido' }),
+    net_area: z.coerce.number({ message: 'Ingrese un número válido' }),
+})
+export type ProjectCreateType = z.infer<typeof projectCreateSchema>
 
 export default function CreateProjectDrawer() {
+    const form = useAppForm({
+        defaultValues: {
+            name: '',
+            is_active: true,
+            gross_area: 0,
+            net_area: 0,
+        } satisfies ProjectCreateType as ProjectCreateType,
+        validators: {
+            // @ts-expect-error
+            onSubmit: projectCreateSchema,
+        },
+        onSubmit: ({ value }) => {
+            console.log('submit', value)
+        },
+    })
+
     return (
-        <Button variant='ghost' className='mb-3'>
-            <PlusIcon size={10} />
-            Crear Proyecto
-        </Button>
+        <Drawer direction='right'>
+            <DrawerTrigger asChild>
+                <Button variant='ghost' className='mb-3'>
+                    <PlusIcon size={10} />
+                    Crear Proyecto
+                </Button>
+            </DrawerTrigger>
+            <DrawerContent>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        form.handleSubmit()
+                    }}
+                >
+                    <DrawerHeader>
+                        <DrawerTitle>Crear Proyecto</DrawerTitle>
+                        <DrawerDescription>
+                            Crea un nuevo proyecto con la información
+                            proporcionada.
+                        </DrawerDescription>
+                    </DrawerHeader>
+
+                    <FieldGroup className='my-5 px-4'>
+                        <FieldSet>
+                            <form.AppField name='name'>
+                                {(field) => (
+                                    <field.TextField
+                                        label='Nombre'
+                                        placeholder='Ingrese el nombre del proyecto'
+                                        name='name'
+                                    />
+                                )}
+                            </form.AppField>
+                            <form.AppField name='gross_area'>
+                                {(field) => (
+                                    <field.TextField
+                                        label='Area Bruta'
+                                        placeholder='Ingrese al area bruta'
+                                        name='gross_area'
+                                    />
+                                )}
+                            </form.AppField>
+                            <form.AppField name='net_area'>
+                                {(field) => (
+                                    <field.TextField
+                                        label='Area Neta'
+                                        placeholder='Ingrese al area bruta'
+                                        name='net_area'
+                                    />
+                                )}
+                            </form.AppField>
+                        </FieldSet>
+                    </FieldGroup>
+
+                    <DrawerFooter>
+                        <Field orientation='horizontal'>
+                            <form.AppForm>
+                                <form.FormButton label='Guardar' />
+                            </form.AppForm>
+                            <DrawerClose asChild>
+                                <Button variant='outline'>Cancel</Button>
+                            </DrawerClose>
+                        </Field>
+                    </DrawerFooter>
+                </form>
+            </DrawerContent>
+        </Drawer>
     )
 }
