c960c5dce555fb30c3a131344c55b69d246036d2
Andres Court <andres@andrescourt.com>
2025-12-14 17:34:24 -0500

    feat(projects): projects page structure

--- src/routes/_authed/parametros/proyectos.tsx
+++ src/routes/_authed/parametros/proyectos.tsx
@@ -1,9 +1,84 @@
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
+import {
+    getAllProjects,
+    type ProjectResponseType,
+} from '@/queries/parametros/projects'
+import type { ColumnDef } from '@tanstack/react-table'
+import { PageTitle } from '@/components/pages/Title'
+import DataTable from '@/components/table/DataTable'
+import { PencilIcon, PlusIcon } from 'lucide-react'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/_authed/parametros/proyectos')({
-  component: RouteComponent,
+    component: RouteComponent,
+    loader: ({ context: { queryClient } }) => {
+        queryClient.ensureQueryData({
+            queryKey: ['proyectos'],
+            queryFn: getAllProjects,
+        })
+    },
 })
 
 function RouteComponent() {
-  return <div>Hello "/_authed/parametros/proyectos"!</div>
+    const { data } = useSuspenseQuery({
+        queryKey: ['proyectos'],
+        queryFn: getAllProjects,
+    })
+
+    const columns: ColumnDef<ProjectResponseType>[] = [
+        {
+            header: 'Nombre',
+            accessorKey: 'name',
+        },
+        {
+            header: 'Area Bruta',
+            size: 200,
+            accessorKey: 'gross_area',
+            cell: ({ row }) => {
+                return (
+                    <span className='block text-right'>
+                        {row.original.gross_area.toLocaleString('es-EC', {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                        })}
+                    </span>
+                )
+            },
+        },
+        {
+            header: 'Area Neta',
+            accessorKey: 'net_area',
+            cell: ({ row }) => {
+                return (
+                    <span className='block text-right'>
+                        {row.original.net_area.toLocaleString('es-EC', {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                        })}
+                    </span>
+                )
+            },
+        },
+        {
+            header: '',
+            accessorKey: 'actions',
+            cell: () => {
+                return <PencilIcon className='text-warning' size={16} />
+            },
+        },
+    ]
+
+    return (
+        <div>
+            <PageTitle title='Proyectos' />
+
+            <Button variant='ghost' className='mb-3'>
+                <PlusIcon size={10} />
+                Crear Proyecto
+            </Button>
+
+            <DataTable data={data} columns={columns} />
+        </div>
+    )
 }
