529b29c32e6995ab3cb343eb053d0ddc4380a9bb
Andres Court <andres@andrescourt.com>
2025-12-18 22:07:48 -0500

    feat(supplier): save the supplier to the database

--- src/components/drawers/parametros/proveedores/create-supplier.tsx
+++ src/components/drawers/parametros/proveedores/create-supplier.tsx
@@ -1,5 +1,7 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { PlusIcon } from 'lucide-react'
 import { useEffect, useState } from 'react'
+import { toast } from 'sonner'
 import { Button } from '@/components/ui/button'
 import {
     Drawer,
@@ -14,11 +16,13 @@ import {
 import { Field, FieldGroup, FieldSet } from '@/components/ui/field'
 import { useAppForm } from '@/hooks/formHook'
 import {
-    supplierCreateSchema,
+    createSupplier,
     type SupplierCreateType,
+    supplierCreateSchema,
 } from '@/queries/parametros/proveedores'
 
 export default function CreateSupplier() {
+    const queryClient = useQueryClient()
     const [open, setOpen] = useState(false)
     const form = useAppForm({
         defaultValues: {
@@ -32,8 +36,34 @@ export default function CreateSupplier() {
             onSubmit: supplierCreateSchema,
         },
         onSubmit: ({ value }) => {
-            console.log(value)
-            // mutation.mutate(value)
+            mutation.mutate(value)
+        },
+    })
+
+    const mutation = useMutation({
+        mutationFn: (data: SupplierCreateType) =>
+            createSupplier({ data: { data } }),
+        onSuccess: () => {
+            toast.success('Proveedor creado con exito')
+            queryClient.invalidateQueries({
+                queryKey: ['proveedores'],
+            })
+            setOpen(false)
+        },
+        onError: (error) => {
+            const e = JSON.parse(error.message)
+            if (e.code === 409) {
+                toast.error(e.data.message, {
+                    richColors: true,
+                    position: 'top-center',
+                })
+                return
+            }
+
+            toast.error('Error al crear el proveedor', {
+                richColors: true,
+                position: 'top-center',
+            })
         },
     })
 
