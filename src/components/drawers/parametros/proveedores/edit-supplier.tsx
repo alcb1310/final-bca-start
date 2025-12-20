2f3752e80a60e901e7dc58b2793cbf44ba32d16c
Andres Court <andres@andrescourt.com>
2025-12-19 20:32:20 -0500

    feat(suppliers): show suppliers edit form

--- src/components/drawers/parametros/proveedores/edit-supplier.tsx
+++ src/components/drawers/parametros/proveedores/edit-supplier.tsx
@@ -1,8 +1,10 @@
 import { PencilIcon } from 'lucide-react'
 import {
     Drawer,
+    DrawerClose,
     DrawerContent,
     DrawerDescription,
+    DrawerFooter,
     DrawerHeader,
     DrawerTitle,
     DrawerTrigger,
@@ -13,6 +15,7 @@ import {
     supplierEditSchema,
     type SupplierEditType,
 } from '@/queries/parametros/proveedores'
+import { Field, FieldGroup, FieldSet } from '@/components/ui/field'
 
 interface EditSupplierProps {
     supplier: SupplierEditType
@@ -52,6 +55,65 @@ export default function EditSupplier({
                             Edita el proveedor con la información ingresada
                         </DrawerDescription>
                     </DrawerHeader>
+                    <FieldGroup className='my-5 px-4'>
+                        <FieldSet>
+                            <form.AppField name='name'>
+                                {(field) => (
+                                    <field.TextField
+                                        label='Nombre'
+                                        placeholder='Ingrese el nombre del proveedor'
+                                        name='name'
+                                    />
+                                )}
+                            </form.AppField>
+                            <form.AppField name='supplier_id'>
+                                {(field) => (
+                                    <field.TextField
+                                        label='RUC'
+                                        placeholder='Ingrese el RUC del proveedor'
+                                        name='supplier_id'
+                                    />
+                                )}
+                            </form.AppField>
+                            <form.AppField name='contact_name'>
+                                {(field) => (
+                                    <field.TextField
+                                        label='Nombre del Contacto'
+                                        placeholder='Nombre del contacto'
+                                        name='contact_name'
+                                    />
+                                )}
+                            </form.AppField>
+                            <form.AppField name='contact_email'>
+                                {(field) => (
+                                    <field.TextField
+                                        label='Email del Contacto'
+                                        placeholder='Email del contacto'
+                                        name='contact_email'
+                                    />
+                                )}
+                            </form.AppField>
+                            <form.AppField name='contact_phone'>
+                                {(field) => (
+                                    <field.TextField
+                                        label='Teléfono del Contacto'
+                                        placeholder='Teléfono del contacto'
+                                        name='contact_phone'
+                                    />
+                                )}
+                            </form.AppField>
+                        </FieldSet>
+                    </FieldGroup>
+                    <DrawerFooter>
+                        <Field orientation='horizontal'>
+                            <form.AppForm>
+                                <form.FormButton label='Guardar' />
+                            </form.AppForm>
+                            <DrawerClose asChild>
+                                <Button variant='outline'>Cancel</Button>
+                            </DrawerClose>
+                        </Field>
+                    </DrawerFooter>
                 </form>
             </DrawerContent>
         </Drawer>
