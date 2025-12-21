89d25c85ff8b8ed19445c05029342e44632b5ba5
Andres Court <andres@andrescourt.com>
2025-12-21 11:00:17 -0500

    feat(budget-items): open a drawer for the create budget item

--- src/components/drawers/parametros/partidas/create-item.tsx
+++ src/components/drawers/parametros/partidas/create-item.tsx
@@ -1,13 +1,44 @@
 import { PlusIcon } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from '@/components/ui/drawer'
+import { Field } from '@/components/ui/field'
 
 export default function CreateItemDrawer() {
     return (
-        <div>
-            <Button variant='default' size='sm'>
-                <PlusIcon size={10} />
-                Crear Partida
-            </Button>
-        </div>
+        <Drawer direction='right'>
+            <DrawerTrigger asChild>
+                <Button variant='default' size='sm'>
+                    <PlusIcon size={10} />
+                    Crear Partida
+                </Button>
+            </DrawerTrigger>
+            <DrawerContent>
+                <DrawerHeader>
+                    <DrawerTitle>Crear Partida</DrawerTitle>
+                    <DrawerDescription>
+                        Crea una partida presupuestaria con la información
+                        requerida.
+                    </DrawerDescription>
+                </DrawerHeader>
+                <form>
+                    <DrawerFooter>
+                        <Field orientation='horizontal'>
+                            <DrawerClose asChild>
+                                <Button variant='outline'>Cancel</Button>
+                            </DrawerClose>
+                        </Field>
+                    </DrawerFooter>
+                </form>
+            </DrawerContent>
+        </Drawer>
     )
 }
