91a2eac26fdc3abb65f89731391d3d35cbd1d86a
Andres Court <andres@andrescourt.com>
2025-12-20 22:00:19 -0500

    feat(budget-items): budget items response type

---
+++ src/queries/parametros/partidas.ts
@@ -0,0 +1,13 @@
+import z from 'zod'
+import { stringNull } from '@/types/StringNull'
+
+export const budgetItemResponseSchema = z.object({
+    id: z.number(),
+    code: z.string(),
+    name: z.string(),
+    parent_id: z.string().nullable(),
+    parent_code: stringNull,
+    parent_name: stringNull,
+})
+
+export type BudgetItemResponse = z.infer<typeof budgetItemResponseSchema>
