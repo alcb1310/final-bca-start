0bcdee2bc5fa7064e6e3a115b534f466b95b6852
Andres Court <andres@andrescourt.com>
2025-12-20 22:24:28 -0500

    feat(budget-items): get all budget items queries

--- src/queries/parametros/partidas.ts
+++ src/queries/parametros/partidas.ts
@@ -1,8 +1,11 @@
+import { createServerFn } from '@tanstack/react-start'
 import z from 'zod'
 import { stringNull } from '@/types/StringNull'
 
+const url = process.env.BACKEND_URL
+
 export const budgetItemResponseSchema = z.object({
-    id: z.number(),
+    id: z.string().uuid(),
     code: z.string(),
     name: z.string(),
     accumulate: z.boolean(),
@@ -12,3 +15,16 @@ export const budgetItemResponseSchema = z.object({
 })
 
 export type BudgetItemResponse = z.infer<typeof budgetItemResponseSchema>
+
+export const getAllBudgetItems = createServerFn({ method: 'GET' }).handler(
+    async () => {
+        const response = await fetch(`${url}/budget-items`)
+        const data = await response.json()
+
+        if (!response.ok) {
+            throw new Error(data.message)
+        }
+
+        return data as BudgetItemResponse[]
+    },
+)
