1edfe7faab2237f7a691d2080dbbe5680ad92146
Andres Court <andres@andrescourt.com>
2025-12-25 20:55:10 -0500

    feat(categories): Display categories columns

--- src/routes/_authed/parametros/categorias.tsx
+++ src/routes/_authed/parametros/categorias.tsx
@@ -1,14 +1,30 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { PlusIcon } from 'lucide-react'
+import type { ColumnDef } from '@tanstack/react-table'
+import { PencilIcon, PlusIcon } from 'lucide-react'
 import { PageTitle } from '@/components/pages/Title'
 import DataTable from '@/components/table/DataTable'
 import { Button } from '@/components/ui/button'
+import type { CategoryType } from '@/queries/parametros/categories'
 
 export const Route = createFileRoute('/_authed/parametros/categorias')({
     component: RouteComponent,
 })
 
 function RouteComponent() {
+    const columns: ColumnDef<CategoryType>[] = [
+        {
+            header: 'Nombre',
+            accessorKey: 'name',
+        },
+        {
+            header: '',
+            accessorKey: 'actions',
+            cell: ({ row }) => {
+                return <PencilIcon size={10} />
+            },
+        },
+    ]
+
     return (
         <div>
             <PageTitle title='Categorias' />
@@ -18,7 +34,7 @@ function RouteComponent() {
                 Crear Categoría
             </Button>
 
-            <DataTable columns={[]} data={[]} />
+            <DataTable columns={columns} data={[]} />
         </div>
     )
 }
