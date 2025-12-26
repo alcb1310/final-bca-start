b98e866e3d4b802f678b499da49e0aecf146c501
Andres Court <andres@andrescourt.com>
2025-12-25 21:51:46 -0500

    feat(category): display the form in the drawer

--- src/components/drawers/parametros/categorias/create-category.tsx
+++ src/components/drawers/parametros/categorias/create-category.tsx
@@ -2,12 +2,15 @@ import { PlusIcon } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import {
     Drawer,
+    DrawerClose,
     DrawerContent,
     DrawerDescription,
+    DrawerFooter,
     DrawerHeader,
     DrawerTitle,
     DrawerTrigger,
 } from '@/components/ui/drawer'
+import { Field, FieldGroup, FieldSet } from '@/components/ui/field'
 import { useAppForm } from '@/hooks/formHook'
 import {
     type CreateCategoryType,
@@ -50,6 +53,31 @@ export default function CreateCategoryDrawer() {
                             requerida
                         </DrawerDescription>
                     </DrawerHeader>
+
+                    <FieldGroup className='my-5 px-4'>
+                        <FieldSet>
+                            <form.AppField name='name'>
+                                {(field) => (
+                                    <field.TextField
+                                        label='Nombre'
+                                        placeholder='Ingrese el nombre de la categoría'
+                                        name='name'
+                                    />
+                                )}
+                            </form.AppField>
+                        </FieldSet>
+                    </FieldGroup>
+
+                    <DrawerFooter>
+                        <Field orientation='horizontal'>
+                            <form.AppForm>
+                                <form.FormButton label='Guardar' />
+                            </form.AppForm>
+                            <DrawerClose asChild>
+                                <Button variant='outline'>Cancel</Button>
+                            </DrawerClose>
+                        </Field>
+                    </DrawerFooter>
                 </form>
             </DrawerContent>
         </Drawer>
