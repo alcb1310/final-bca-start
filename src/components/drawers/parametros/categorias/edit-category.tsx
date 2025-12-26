16e83baf979c66be94503351a3c0a4700b069881
Andres Court <andres@andrescourt.com>
2025-12-26 17:27:50 -0500

    feat(category): display the edit category form

--- src/components/drawers/parametros/categorias/edit-category.tsx
+++ src/components/drawers/parametros/categorias/edit-category.tsx
@@ -2,12 +2,15 @@ import { PencilIcon } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import {
     Drawer,
+    DrawerClose,
     DrawerContent,
     DrawerDescription,
+    DrawerFooter,
     DrawerHeader,
     DrawerTitle,
     DrawerTrigger,
 } from '@/components/ui/drawer'
+import { Field, FieldGroup, FieldSet } from '@/components/ui/field'
 import { useAppForm } from '@/hooks/formHook'
 import {
     type CategoryType,
@@ -51,6 +54,29 @@ export default function EditCategory({
                             Edita la categoría con la información proporcionada
                         </DrawerDescription>
                     </DrawerHeader>
+                    <FieldGroup className='my-5 px-4'>
+                        <FieldSet>
+                            <form.AppField name='name'>
+                                {(field) => (
+                                    <field.TextField
+                                        label='Nombre'
+                                        placeholder='Ingrese el nombre del proyecto'
+                                        name='name'
+                                    />
+                                )}
+                            </form.AppField>
+                        </FieldSet>
+                    </FieldGroup>
+                    <DrawerFooter>
+                        <Field orientation='horizontal'>
+                            <form.AppForm>
+                                <form.FormButton label='Guardar' />
+                            </form.AppForm>
+                            <DrawerClose asChild>
+                                <Button variant='outline'>Cancel</Button>
+                            </DrawerClose>
+                        </Field>
+                    </DrawerFooter>
                 </form>
             </DrawerContent>
         </Drawer>
