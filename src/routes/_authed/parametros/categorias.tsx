5dca243931afebbea896f67f2ddeff45d7fc883e
Andres Court <andres@andrescourt.com>
2025-12-25 20:59:44 -0500

    feat(category): display all the categories

--- src/routes/_authed/parametros/categorias.tsx
+++ src/routes/_authed/parametros/categorias.tsx
@@ -1,16 +1,31 @@
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 import type { ColumnDef } from '@tanstack/react-table'
 import { PencilIcon, PlusIcon } from 'lucide-react'
 import { PageTitle } from '@/components/pages/Title'
 import DataTable from '@/components/table/DataTable'
 import { Button } from '@/components/ui/button'
-import type { CategoryType } from '@/queries/parametros/categories'
+import {
+    type CategoryType,
+    getAllCategories,
+} from '@/queries/parametros/categories'
 
 export const Route = createFileRoute('/_authed/parametros/categorias')({
     component: RouteComponent,
+    loader: ({ context: { queryClient } }) => {
+        queryClient.ensureQueryData({
+            queryKey: ['categorias'],
+            queryFn: getAllCategories,
+        })
+    },
 })
 
 function RouteComponent() {
+    const { data } = useSuspenseQuery({
+        queryKey: ['categorias'],
+        queryFn: getAllCategories,
+    })
+
     const columns: ColumnDef<CategoryType>[] = [
         {
             header: 'Nombre',
@@ -34,7 +49,7 @@ function RouteComponent() {
                 Crear Categoría
             </Button>
 
-            <DataTable columns={columns} data={[]} />
+            <DataTable columns={columns} data={data} />
         </div>
     )
 }
