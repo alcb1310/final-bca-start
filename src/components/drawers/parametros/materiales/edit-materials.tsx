6d563da8415f8b0cf63ec1a369cbe7fd4ef1061d
Andres Court <andres@andrescourt.com>
2025-12-28 16:08:43 -0500

    feat(materials): display the edit form with the correct values

--- src/components/drawers/parametros/materiales/edit-materials.tsx
+++ src/components/drawers/parametros/materiales/edit-materials.tsx
@@ -2,19 +2,36 @@ import { PencilIcon } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import {
     Drawer,
+    DrawerClose,
     DrawerContent,
     DrawerDescription,
+    DrawerFooter,
     DrawerHeader,
     DrawerTitle,
     DrawerTrigger,
 } from '@/components/ui/drawer'
-import type { MaterialResponseType } from '@/queries/parametros/materiales'
+import { useAppForm } from '@/hooks/formHook'
+import {
+    type MaterialResponseType,
+    materialResponseSchema,
+} from '@/queries/parametros/materiales'
+import { Field, FieldGroup, FieldSet } from '@/components/ui/field'
 
 interface EditMaterialProps {
     material: MaterialResponseType
 }
 
 export function EditMaterial({ material }: Readonly<EditMaterialProps>) {
+    const form = useAppForm({
+        defaultValues: material,
+        validators: {
+            onSubmit: materialResponseSchema,
+        },
+        onSubmit: ({ value }) => {
+            console.log(value)
+        },
+    })
+
     return (
         <Drawer direction='right'>
             <DrawerTrigger asChild>
@@ -23,12 +40,70 @@ export function EditMaterial({ material }: Readonly<EditMaterialProps>) {
                 </Button>
             </DrawerTrigger>
             <DrawerContent>
-                <DrawerHeader>
-                    <DrawerTitle>Editar Material</DrawerTitle>
-                    <DrawerDescription>
-                        Edita el material con la información proporcionada
-                    </DrawerDescription>
-                </DrawerHeader>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        form.handleSubmit()
+                    }}
+                >
+                    <DrawerHeader>
+                        <DrawerTitle>Editar Material</DrawerTitle>
+                        <DrawerDescription>
+                            Edita el material con la información proporcionada
+                        </DrawerDescription>
+                    </DrawerHeader>
+                    <FieldGroup className='my-5 px-4'>
+                        <FieldSet>
+                            <form.AppField name='code'>
+                                {(field) => (
+                                    <field.TextField
+                                        label='Código'
+                                        placeholder='Ingrese el código del material'
+                                        name='code'
+                                    />
+                                )}
+                            </form.AppField>
+                            <form.AppField name='name'>
+                                {(field) => (
+                                    <field.TextField
+                                        label='Nombre'
+                                        placeholder='Ingrese el nombre del material'
+                                        name='name'
+                                    />
+                                )}
+                            </form.AppField>
+                            <form.AppField name='unit'>
+                                {(field) => (
+                                    <field.TextField
+                                        label='Unidad'
+                                        placeholder='Ingrese la unidad del material'
+                                        name='unit'
+                                    />
+                                )}
+                            </form.AppField>
+                            <form.AppField name='category.name'>
+                                {(field) => (
+                                    <field.TextField
+                                        label='Categoría'
+                                        name='category.name'
+                                        disabled={true}
+                                    />
+                                )}
+                            </form.AppField>
+                        </FieldSet>
+                    </FieldGroup>
+                    <DrawerFooter>
+                        <Field orientation='horizontal'>
+                            <form.AppForm>
+                                <form.FormButton label='Guardar' />
+                            </form.AppForm>
+                            <DrawerClose asChild>
+                                <Button variant='outline'>Cancel</Button>
+                            </DrawerClose>
+                        </Field>
+                    </DrawerFooter>
+                </form>
             </DrawerContent>
         </Drawer>
     )
