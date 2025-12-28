9b910b7b26ac0b126b8555cf1fa26c644fc840ad
Andres Court <andres@andrescourt.com>
2025-12-27 21:05:46 -0500

    feat(materials): list all materials page layout

--- src/routes/_authed/parametros/materiales.tsx
+++ src/routes/_authed/parametros/materiales.tsx
@@ -1,9 +1,27 @@
 import { createFileRoute } from '@tanstack/react-router'
+import type { ColumnDef } from '@tanstack/react-table'
+import { PlusIcon } from 'lucide-react'
+import { PageTitle } from '@/components/pages/Title'
+import DataTable from '@/components/table/DataTable'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/_authed/parametros/materiales')({
-  component: RouteComponent,
+    component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/_authed/parametros/materiales"!</div>
+    const columns: ColumnDef<any>[] = []
+
+    return (
+        <div>
+            <PageTitle title='Materiales' />
+
+            <Button variant={'default'} size={'sm'}>
+                <PlusIcon size={10} />
+                Crear Material
+            </Button>
+
+            <DataTable columns={columns} data={[]} />
+        </div>
+    )
 }
