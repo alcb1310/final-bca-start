c6b2524969356e31a7edde4ac50c3dfe5b218117
Andres Court <andres@andrescourt.com>
2025-12-27 21:23:58 -0500

    feat(materials): load data from the backend

--- src/routes/_authed/parametros/materiales.tsx
+++ src/routes/_authed/parametros/materiales.tsx
@@ -4,13 +4,28 @@ import { PencilIcon, PlusIcon, TrashIcon } from 'lucide-react'
 import { PageTitle } from '@/components/pages/Title'
 import DataTable from '@/components/table/DataTable'
 import { Button } from '@/components/ui/button'
-import type { MaterialResponseType } from '@/queries/parametros/materiales'
+import {
+    getAllMaterials,
+    type MaterialResponseType,
+} from '@/queries/parametros/materiales'
+import { useSuspenseQuery } from '@tanstack/react-query'
 
 export const Route = createFileRoute('/_authed/parametros/materiales')({
     component: RouteComponent,
+    loader: ({ context: { queryClient } }) => {
+        queryClient.ensureQueryData({
+            queryKey: ['materiales'],
+            queryFn: getAllMaterials,
+        })
+    },
 })
 
 function RouteComponent() {
+    const { data } = useSuspenseQuery({
+        queryKey: ['materiales'],
+        queryFn: getAllMaterials,
+    })
+
     const columns: ColumnDef<MaterialResponseType>[] = [
         {
             header: 'Categoría',
@@ -35,7 +50,7 @@ function RouteComponent() {
                 return (
                     <div className='flex gap-2'>
                         <PencilIcon size={10} className='text-warning' />
-                        <TrashIcon size={10} className='text-danger' />
+                        <TrashIcon size={10} className='text-destructive' />
                     </div>
                 )
             },
@@ -51,7 +66,7 @@ function RouteComponent() {
                 Crear Material
             </Button>
 
-            <DataTable columns={columns} data={[]} />
+            <DataTable columns={columns} data={data} />
         </div>
     )
 }
