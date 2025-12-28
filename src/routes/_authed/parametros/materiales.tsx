9403b588fc214ef81d0b27723ac660e81c7dba74
Andres Court <andres@andrescourt.com>
2025-12-27 21:13:22 -0500

    feat(materiales): add materials list column headers

--- src/routes/_authed/parametros/materiales.tsx
+++ src/routes/_authed/parametros/materiales.tsx
@@ -1,16 +1,46 @@
 import { createFileRoute } from '@tanstack/react-router'
 import type { ColumnDef } from '@tanstack/react-table'
-import { PlusIcon } from 'lucide-react'
+import { PencilIcon, PlusIcon, TrashIcon } from 'lucide-react'
 import { PageTitle } from '@/components/pages/Title'
 import DataTable from '@/components/table/DataTable'
 import { Button } from '@/components/ui/button'
+import type { MaterialResponseType } from '@/queries/parametros/materiales'
 
 export const Route = createFileRoute('/_authed/parametros/materiales')({
     component: RouteComponent,
 })
 
 function RouteComponent() {
-    const columns: ColumnDef<any>[] = []
+    const columns: ColumnDef<MaterialResponseType>[] = [
+        {
+            header: 'Categoría',
+            accessorKey: 'category.name',
+        },
+        {
+            header: 'Código',
+            accessorKey: 'code',
+        },
+        {
+            header: 'Nombre',
+            accessorKey: 'name',
+        },
+        {
+            header: 'Unidad',
+            accessorKey: 'unit',
+        },
+        {
+            header: '',
+            accessorKey: 'actions',
+            cell: ({ row }) => {
+                return (
+                    <div className='flex gap-2'>
+                        <PencilIcon size={10} className='text-warning' />
+                        <TrashIcon size={10} className='text-danger' />
+                    </div>
+                )
+            },
+        },
+    ]
 
     return (
         <div>
