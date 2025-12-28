64fdb180051f86573829d5a32ac0f070960ff4c2
Andres Court <andres@andrescourt.com>
2025-12-27 21:18:16 -0500

    feat(materials): fetch all materials from backend

--- src/queries/parametros/materiales.ts
+++ src/queries/parametros/materiales.ts
@@ -1,5 +1,8 @@
+import { createServerFn } from '@tanstack/react-start'
 import z from 'zod'
 import { categorySchema } from './categories'
+
+const url = process.env.BACKEND_URL
 
 export const materialResponseSchema = z.object({
     id: z.string().uuid({ version: 'v4' }),
@@ -10,3 +13,16 @@ export const materialResponseSchema = z.object({
 })
 
 export type MaterialResponseType = z.infer<typeof materialResponseSchema>
+
+export const getAllMaterials = createServerFn({ method: 'GET' }).handler(
+    async () => {
+        const response = await fetch(`${url}/materials`)
+        const data = await response.json()
+
+        if (!response.ok) {
+            throw new Error(data.message)
+        }
+
+        return data as MaterialResponseType[]
+    },
+)
