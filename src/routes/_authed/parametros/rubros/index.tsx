bab067de93a7691bc9d56b883eb70e58a42d032e
Andres Court <andres@andrescourt.com>
2025-12-29 17:30:45 -0500

    feat(items): display the columns in the page

--- src/routes/_authed/parametros/rubros/index.tsx
+++ src/routes/_authed/parametros/rubros/index.tsx
@@ -1,16 +1,39 @@
 import { createFileRoute } from '@tanstack/react-router'
 import type { ColumnDef } from '@tanstack/react-table'
-import { PlusIcon } from 'lucide-react'
+import { PencilIcon, PlusIcon } from 'lucide-react'
 import { PageTitle } from '@/components/pages/Title'
 import DataTable from '@/components/table/DataTable'
 import { Button } from '@/components/ui/button'
+import type { RubroResponseType } from '@/queries/parametros/rubros'
 
 export const Route = createFileRoute('/_authed/parametros/rubros/')({
     component: RouteComponent,
 })
 
 function RouteComponent() {
-    const columns: ColumnDef<any>[] = []
+    const columns: ColumnDef<RubroResponseType>[] = [
+        {
+            header: 'Código',
+            accessorKey: 'code',
+        },
+        {
+            header: 'Nombre',
+            accessorKey: 'name',
+        },
+        {
+            header: 'Unidad',
+            accessorKey: 'unit',
+        },
+        {
+            header: '',
+            accessorKey: 'actions',
+            cell: ({ row }) => (
+                <div>
+                    <PencilIcon size={10} className='text-warning' />
+                </div>
+            ),
+        },
+    ]
     return (
         <div>
             <PageTitle title='Rubros' />
