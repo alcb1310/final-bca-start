869cce671ee93a25a0e4975875d1a88c78c1aabd
Andres Court <andres@andrescourt.com>
2025-12-29 17:18:37 -0500

    feat(items): items page layout

--- src/routes/_authed/parametros/rubros/index.tsx
+++ src/routes/_authed/parametros/rubros/index.tsx
@@ -1,9 +1,26 @@
 import { createFileRoute } from '@tanstack/react-router'
+import type { ColumnDef } from '@tanstack/react-table'
+import { PlusIcon } from 'lucide-react'
+import { PageTitle } from '@/components/pages/Title'
+import DataTable from '@/components/table/DataTable'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/_authed/parametros/rubros/')({
-  component: RouteComponent,
+    component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/_authed/parametros/rubros/"!</div>
+    const columns: ColumnDef<any>[] = []
+    return (
+        <div>
+            <PageTitle title='Rubros' />
+
+            <Button>
+                <PlusIcon size={10} />
+                Crear Rubro
+            </Button>
+
+            <DataTable columns={columns} data={[]} />
+        </div>
+    )
 }
