8c98f08ab9dc56463cf983046c0c44b71e477e84
Andres Court <andres@andrescourt.com>
2025-12-30 21:53:20 -0500

    feat(items): validate that the parameter is a uuid

--- src/routes/_authed/parametros/rubros/$rubroId.tsx
+++ src/routes/_authed/parametros/rubros/$rubroId.tsx
@@ -1,9 +1,22 @@
 import { createFileRoute } from '@tanstack/react-router'
+import z from 'zod'
 
 export const Route = createFileRoute('/_authed/parametros/rubros/$rubroId')({
-  component: RouteComponent,
+    component: RouteComponent,
+    params: {
+        parse: (rawParams) => {
+            try {
+                const id = z.uuid().parse(rawParams.rubroId)
+                return { rubroId: id }
+            } catch (error) {
+                throw new Error(`Invalid rubroId: ${error}`)
+            }
+        },
+    },
 })
 
 function RouteComponent() {
-  return <div>Hello "/_authed/parametros/rubros/$rubroId"!</div>
+    const { rubroId } = Route.useParams()
+
+    return <div>Hello "/_authed/parametros/rubros/${rubroId}!</div>
 }
