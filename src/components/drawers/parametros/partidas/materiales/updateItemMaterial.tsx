d3df4cab278d14e1417220d0e89ea61065500250
Andres Court <andres@andrescourt.com>
2026-01-04 22:01:21 -0500

    feat(item-materials): added the edit material form

--- src/components/drawers/parametros/partidas/materiales/updateItemMaterial.tsx
+++ src/components/drawers/parametros/partidas/materiales/updateItemMaterial.tsx
@@ -9,14 +9,34 @@ import {
     DrawerTitle,
     DrawerTrigger,
 } from '@/components/ui/drawer'
-import { Field } from '@/components/ui/field'
-import type { RubroMaterialType } from '@/queries/parametros/rubros'
+import { Field, FieldGroup, FieldSet } from '@/components/ui/field'
+import { useAppForm } from '@/hooks/formHook'
+import {
+    createRubroMaterialSchema,
+    type RubroMaterialType,
+} from '@/queries/parametros/rubros'
 
 type UpdateItemMaterialProps = {
     itemMaterial: RubroMaterialType
 }
 
 export function UpdateItemMaterial({ itemMaterial }: UpdateItemMaterialProps) {
+    const form = useAppForm({
+        defaultValues: {
+            item_id: itemMaterial.item_id,
+            material_id: itemMaterial.material_id,
+            material_name: itemMaterial.material_name,
+            quantity: itemMaterial.quantity,
+        },
+        validators: {
+            // @ts-expect-error: TODO fix typescript error
+            onSubmit: createRubroMaterialSchema,
+        },
+        onSubmit: ({ value }) => {
+            console.log(value)
+        },
+    })
+
     return (
         <Drawer direction='right'>
             <DrawerTrigger asChild>
@@ -25,13 +45,45 @@ export function UpdateItemMaterial({ itemMaterial }: UpdateItemMaterialProps) {
                 </Button>
             </DrawerTrigger>
             <DrawerContent className='px-4'>
-                <form>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        form.handleSubmit()
+                    }}
+                >
                     <DrawerTitle>Editar Material</DrawerTitle>
                     <DrawerDescription>
                         Edita el material asignado al rubro
                     </DrawerDescription>
+                    <FieldGroup className='my-5'>
+                        <FieldSet>
+                            <form.AppField name='material_name'>
+                                {(field) => (
+                                    <field.TextField
+                                        label='Material'
+                                        placeholder='Ingrese la cantidad'
+                                        name='Material'
+                                        disabled
+                                    />
+                                )}
+                            </form.AppField>
+                            <form.AppField name='quantity'>
+                                {(field) => (
+                                    <field.TextField
+                                        label='Cantidad'
+                                        placeholder='Ingrese la cantidad'
+                                        name='quantity'
+                                    />
+                                )}
+                            </form.AppField>
+                        </FieldSet>
+                    </FieldGroup>
                     <DrawerFooter>
                         <Field orientation={'horizontal'}>
+                            <form.AppForm>
+                                <form.FormButton label='Guardar' />
+                            </form.AppForm>
                             <DrawerClose asChild>
                                 <Button variant={'outline'}>Cancelar</Button>
                             </DrawerClose>
