2dc47e2d2cc4a8d86decef4580f0c1c3c3a48a9f
Andres Court <andres@andrescourt.com>
2026-01-04 21:51:41 -0500

    feat(item-materials): edit material drawer description

--- src/components/drawers/parametros/partidas/materiales/updateItemMaterial.tsx
+++ src/components/drawers/parametros/partidas/materiales/updateItemMaterial.tsx
@@ -1,25 +1,44 @@
 import { PencilIcon } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer'
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerTitle,
+    DrawerTrigger,
+} from '@/components/ui/drawer'
+import { Field } from '@/components/ui/field'
+import type { RubroMaterialType } from '@/queries/parametros/rubros'
 
 type UpdateItemMaterialProps = {
-    itemMaterial: {
-        item_id: string
-        material_id: string
-        material_name: string
-        quantitty: number
-    }
+    itemMaterial: RubroMaterialType
 }
 
 export function UpdateItemMaterial({ itemMaterial }: UpdateItemMaterialProps) {
     return (
-        <Drawer>
+        <Drawer direction='right'>
             <DrawerTrigger asChild>
-                <Button size={'icon-sm'}>
+                <Button size={'icon-sm'} variant={'ghost'}>
                     <PencilIcon className='text-warning' />
                 </Button>
             </DrawerTrigger>
-            <DrawerContent></DrawerContent>
+            <DrawerContent className='px-4'>
+                <form>
+                    <DrawerTitle>Editar Material</DrawerTitle>
+                    <DrawerDescription>
+                        Edita el material asignado al rubro
+                    </DrawerDescription>
+                    <DrawerFooter>
+                        <Field orientation={'horizontal'}>
+                            <DrawerClose asChild>
+                                <Button variant={'outline'}>Cancelar</Button>
+                            </DrawerClose>
+                        </Field>
+                    </DrawerFooter>
+                </form>
+            </DrawerContent>
         </Drawer>
     )
 }
