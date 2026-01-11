8d1484aa4ff51886a63f6cf1e7731a825833b3fa
Andres Court <andres@andrescourt.com>
2026-01-11 17:14:55 -0500

    feat(budgets): list budgets page layout

--- src/routes/_authed/transacciones/presupuesto.tsx
+++ src/routes/_authed/transacciones/presupuesto.tsx
@@ -1,9 +1,28 @@
 import { createFileRoute } from '@tanstack/react-router'
+import type { ColumnDef } from '@tanstack/react-table'
+import { PlusIcon } from 'lucide-react'
+import { PageTitle } from '@/components/pages/Title'
+import DataTable from '@/components/table/DataTable'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/_authed/transacciones/presupuesto')({
-  component: RouteComponent,
+    component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/_authed/transacciones/presupuesto"!</div>
+    const data: any[] = []
+    const columns: ColumnDef<any>[] = []
+
+    return (
+        <div>
+            <PageTitle title='Presupuesto' />
+
+            <Button variant='default' className='mb-3'>
+                <PlusIcon size={10} />
+                Crear Presupuesto
+            </Button>
+
+            <DataTable data={data} columns={columns} />
+        </div>
+    )
 }
