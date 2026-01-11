4ca6d4a1123bfbd904920c0ee6c82b5315a554ad
Andres Court <andres@andrescourt.com>
2026-01-11 17:58:52 -0500

    feat(budgets): show the apropriate columns to display the budget

--- src/routes/_authed/transacciones/presupuesto.tsx
+++ src/routes/_authed/transacciones/presupuesto.tsx
@@ -1,11 +1,14 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 import type { ColumnDef } from '@tanstack/react-table'
-import { PlusIcon } from 'lucide-react'
+import { PencilIcon, PlusIcon } from 'lucide-react'
 import { PageTitle } from '@/components/pages/Title'
 import DataTable from '@/components/table/DataTable'
 import { Button } from '@/components/ui/button'
-import { getAllBudgets } from '@/queries/transacciones/presupuesto'
+import {
+    type BudgetResponseType,
+    getAllBudgets,
+} from '@/queries/transacciones/presupuesto'
 
 export const Route = createFileRoute('/_authed/transacciones/presupuesto')({
     component: RouteComponent,
@@ -18,7 +21,82 @@ export const Route = createFileRoute('/_authed/transacciones/presupuesto')({
 })
 
 function RouteComponent() {
-    const columns: ColumnDef<any>[] = []
+    const columns: ColumnDef<BudgetResponseType>[] = [
+        {
+            header: 'Proyecto',
+            accessorKey: 'project.name',
+        },
+        {
+            header: 'Código',
+            accessorKey: 'budget_item.code',
+        },
+        {
+            header: 'Nombre',
+            accessorKey: 'budget_item.name',
+        },
+        {
+            header: 'Cantidad',
+            cell: ({ row }) => {
+                return (
+                    <span className='block text-right'>
+                        {row.original.remaining_quantity.Valid
+                            ? row.original.remaining_quantity.Float64?.toLocaleString(
+                                'es-EC',
+                                {
+                                    minimumFractionDigits: 2,
+                                    maximumFractionDigits: 2,
+                                },
+                            )
+                            : ''}
+                    </span>
+                )
+            },
+        },
+        {
+            header: 'Costo',
+            cell: ({ row }) => {
+                return (
+                    <span className='block text-right'>
+                        {row.original.remaining_cost.Valid
+                            ? row.original.remaining_cost.Float64?.toLocaleString(
+                                'es-EC',
+                                {
+                                    minimumFractionDigits: 2,
+                                    maximumFractionDigits: 2,
+                                },
+                            )
+                            : ''}
+                    </span>
+                )
+            },
+        },
+        {
+            header: 'Total',
+            cell: ({ row }) => {
+                return (
+                    <span className='block text-right'>
+                        {row.original.remaining_total.toLocaleString('es-EC', {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                        })}
+                    </span>
+                )
+            },
+        },
+        {
+            header: '',
+            accessorKey: 'actions',
+            cell: ({ row }) => {
+                return (
+                    <>
+                        {!row.original.budget_item.accumulate && (
+                            <PencilIcon size={10} className='text-warning' />
+                        )}
+                    </>
+                )
+            },
+        },
+    ]
 
     const { data } = useSuspenseQuery({
         queryKey: ['presupuesto'],
