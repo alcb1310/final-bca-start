221240af5825d140ab1f1a4607e2fdd0fa08256e
Andres Court <andres@andrescourt.com>
2026-01-11 17:33:41 -0500

    feat(budgets): call the get all budgets query

--- src/routes/_authed/transacciones/presupuesto.tsx
+++ src/routes/_authed/transacciones/presupuesto.tsx
@@ -1,17 +1,29 @@
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 import type { ColumnDef } from '@tanstack/react-table'
 import { PlusIcon } from 'lucide-react'
 import { PageTitle } from '@/components/pages/Title'
 import DataTable from '@/components/table/DataTable'
 import { Button } from '@/components/ui/button'
+import { getAllBudgets } from '@/queries/transacciones/presupuesto'
 
 export const Route = createFileRoute('/_authed/transacciones/presupuesto')({
     component: RouteComponent,
+    loader: ({ context: { queryClient } }) => {
+        queryClient.ensureQueryData({
+            queryKey: ['presupuesto'],
+            queryFn: getAllBudgets,
+        })
+    },
 })
 
 function RouteComponent() {
-    const data: any[] = []
     const columns: ColumnDef<any>[] = []
+
+    const { data } = useSuspenseQuery({
+        queryKey: ['presupuesto'],
+        queryFn: getAllBudgets,
+    })
 
     return (
         <div>
