c994323b573da61fecf8d9a5a4d6f9ed1ad0beff
Andres Court <andres@andrescourt.com>
2026-01-12 18:34:49 -0500

    feat(budgets): drawer information structure

--- src/components/drawers/transacciones/presupuesto/edit-budget.tsx
+++ src/components/drawers/transacciones/presupuesto/edit-budget.tsx
@@ -1,6 +1,16 @@
 import { PencilIcon } from 'lucide-react'
 import { Button } from '@/components/ui/button'
-import { Drawer, DrawerContent, DrawerTrigger } from '@/components/ui/drawer'
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from '@/components/ui/drawer'
+import { Field } from '@/components/ui/field'
 
 export function EditBudgetDrawer() {
     return (
@@ -10,7 +20,24 @@ export function EditBudgetDrawer() {
                     <PencilIcon size={10} className='text-warning' />
                 </Button>
             </DrawerTrigger>
-            <DrawerContent></DrawerContent>
+            <DrawerContent>
+                <form>
+                    <DrawerHeader>
+                        <DrawerTitle>Editar Presupuesto</DrawerTitle>
+                        <DrawerDescription>
+                            Edita un presupuesto con la información
+                            proporcionada.
+                        </DrawerDescription>
+                    </DrawerHeader>
+                    <DrawerFooter>
+                        <Field orientation={'horizontal'}>
+                            <DrawerClose asChild>
+                                <Button variant='outline'>Cerrar</Button>
+                            </DrawerClose>
+                        </Field>
+                    </DrawerFooter>
+                </form>
+            </DrawerContent>
         </Drawer>
     )
 }
