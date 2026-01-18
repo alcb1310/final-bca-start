92bd8d689ba4ca8382b82bf957ccd2d154c989ba
Andres Court <andres@andrescourt.com>
2026-01-18 09:50:35 -0500

    feat(invoices): get all invoices backend query

--- src/queries/transacciones/facturas.ts
+++ src/queries/transacciones/facturas.ts
@@ -1,6 +1,9 @@
+import { createServerFn } from '@tanstack/react-start'
 import { z } from 'zod'
 import { projectResponseSchema } from '../parametros/projects'
 import { supplierResponseSchema } from '../parametros/proveedores'
+
+const url = process.env.BACKEND_URL
 
 export const facturaResponseSchema = z.object({
     id: z.string().uuid({ version: 'v4' }),
@@ -10,4 +13,15 @@ export const facturaResponseSchema = z.object({
     invoice_date: z.date(),
 })
 
-export type FacturaType = z.infer<typeof facturaResponseSchema>
+export type FacturaResponseType = z.infer<typeof facturaResponseSchema>
+
+export const getFacturas = createServerFn({ method: 'GET' }).handler(
+    async () => {
+        const response = await fetch(`${url}/invoices`)
+        const data = await response.json()
+        if (!response.ok) {
+            throw new Error(data.message)
+        }
+        return data as FacturaResponseType[]
+    },
+)
