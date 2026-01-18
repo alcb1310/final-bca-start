a4506106027a31fdeb406afe142f7d487582dcda
Andres Court <andres@andrescourt.com>
2026-01-17 22:28:02 -0500

    feat(invoices): basic page layout

--- src/routes/_authed/transacciones/facturas/index.tsx
+++ src/routes/_authed/transacciones/facturas/index.tsx
@@ -1,9 +1,28 @@
 import { createFileRoute } from '@tanstack/react-router'
+import type { ColumnDef } from '@tanstack/react-table'
+import { PlusIcon } from 'lucide-react'
+import { PageTitle } from '@/components/pages/Title'
+import DataTable from '@/components/table/DataTable'
+import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/_authed/transacciones/facturas/')({
     component: RouteComponent,
 })
 
 function RouteComponent() {
-    return <div>Hello "/_authed/transacciones/facturas/"!</div>
+    const data: any[] = []
+    const columns: ColumnDef<any>[] = []
+
+    return (
+        <div>
+            <PageTitle title='Facturas' />
+
+            <Button>
+                <PlusIcon size={10} />
+                Crear Factura
+            </Button>
+
+            <DataTable data={data} columns={columns} />
+        </div>
+    )
 }
