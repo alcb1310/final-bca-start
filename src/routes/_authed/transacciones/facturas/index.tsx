31ecbd9ab6aba2c121e8aa9c9b5cc7ea45bb4d09
Andres Court <andres@andrescourt.com>
2026-01-18 09:50:58 -0500

    feat(invoices): call the get all invoices query

--- src/routes/_authed/transacciones/facturas/index.tsx
+++ src/routes/_authed/transacciones/facturas/index.tsx
@@ -1,17 +1,33 @@
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 import type { ColumnDef } from '@tanstack/react-table'
 import { PlusIcon } from 'lucide-react'
 import { PageTitle } from '@/components/pages/Title'
 import DataTable from '@/components/table/DataTable'
 import { Button } from '@/components/ui/button'
+import {
+    type FacturaResponseType,
+    getFacturas,
+} from '@/queries/transacciones/facturas'
 
 export const Route = createFileRoute('/_authed/transacciones/facturas/')({
     component: RouteComponent,
+    loader: ({ context: { queryClient } }) => {
+        return queryClient.ensureQueryData({
+            queryKey: ['facturas'],
+            queryFn: () => getFacturas(),
+        })
+    },
 })
 
 function RouteComponent() {
-    const data: any[] = []
-    const columns: ColumnDef<any>[] = []
+    const { data } = useSuspenseQuery({
+        queryKey: ['facturas'],
+        queryFn: () => getFacturas(),
+    })
+    const columns: ColumnDef<FacturaResponseType>[] = []
+
+    console.log(data)
 
     return (
         <div>
