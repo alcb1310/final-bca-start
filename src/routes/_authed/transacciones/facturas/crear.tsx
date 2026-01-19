28c8dec6ac172ad259dcbde4c6be7dfdb5ab394b
Andres Court <andres@andrescourt.com>
2026-01-18 22:03:08 -0500

    feat(invoices): read projects and suppliers

--- src/routes/_authed/transacciones/facturas/crear.tsx
+++ src/routes/_authed/transacciones/facturas/crear.tsx
@@ -1,9 +1,58 @@
+import { useQueries } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
+import { PageTitle } from '@/components/pages/Title'
+import { getAllProjects } from '@/queries/parametros/projects'
+import { getAllSuppliers } from '@/queries/parametros/proveedores'
 
 export const Route = createFileRoute('/_authed/transacciones/facturas/crear')({
-  component: RouteComponent,
+    component: RouteComponent,
+    loader: ({ context: { queryClient } }) => {
+        queryClient.ensureQueryData({
+            queryKey: ['proveedores'],
+            queryFn: () => getAllSuppliers(),
+        })
+        queryClient.ensureQueryData({
+            queryKey: ['proyectos'],
+            queryFn: () => getAllProjects(),
+        })
+    },
 })
 
 function RouteComponent() {
-  return <div>Hello "/_authed/transacciones/facturas/crear"!</div>
+    const allQueries = useQueries({
+        queries: [
+            {
+                queryKey: ['proyectos'],
+                queryFn: () => getAllProjects(),
+            },
+            {
+                queryKey: ['proveedores'],
+                queryFn: () => getAllSuppliers(),
+            },
+        ],
+    })
+
+    const projects =
+        allQueries[0].data?.map((project: any) => {
+            return {
+                label: project.name,
+                value: project.id,
+            }
+        }) || []
+    projects.unshift({ label: 'Seleccione un proyecto', value: '' })
+
+    const suppliers =
+        allQueries[1].data?.map((supplier: any) => {
+            return {
+                label: supplier.name,
+                value: supplier.id,
+            }
+        }) || []
+    suppliers.unshift({ label: 'Seleccione un proveedor', value: '' })
+
+    return (
+        <div>
+            <PageTitle title='Crear Factura' />
+        </div>
+    )
 }
