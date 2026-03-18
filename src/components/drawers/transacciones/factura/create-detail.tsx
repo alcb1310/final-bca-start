bcbeedc76e70790331f95a4c6a3962b33f4949bd
Andres Court <andres@andrescourt.com>
2026-03-18 18:09:30 -0500

    feat(invoce-details): basic drawer layout with tile description and buttons

--- src/components/drawers/transacciones/factura/create-detail.tsx
+++ src/components/drawers/transacciones/factura/create-detail.tsx
@@ -2,20 +2,43 @@ import { PlusIcon } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import {
     Drawer,
+    DrawerClose,
     DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
     DrawerHeader,
     DrawerTitle,
     DrawerTrigger,
 } from '@/components/ui/drawer'
+import { Field } from '@/components/ui/field'
+import {
+    detalleCreateSchema,
+    type DetalleCreateType,
+} from '@/queries/transacciones/detalle'
+import { useAppForm } from '@/hooks/formHook'
 
 type CreateInvoiceDetailDrawerProps = {
-    projectId: string
+    invoiceId: string
 }
 
 export function CreateInvoiceDetailDrawer({
-    projectId,
+    invoiceId,
 }: CreateInvoiceDetailDrawerProps) {
-    console.log(projectId)
+    const form = useAppForm({
+        defaultValues: {
+            invoice_id: invoiceId,
+            budget_item_id: '',
+            quantity: 0,
+            cost: 0,
+        } satisfies DetalleCreateType as DetalleCreateType,
+        validators: {
+            // @ts-expect-error
+            onSubmit: detalleCreateSchema,
+        },
+        onSubmit: ({ value }) => {
+            console.log(value)
+        },
+    })
 
     return (
         <Drawer direction='right'>
@@ -26,10 +49,29 @@ export function CreateInvoiceDetailDrawer({
                 </Button>
             </DrawerTrigger>
             <DrawerContent>
-                <form>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        form.handleSubmit()
+                    }}
+                >
                     <DrawerHeader>
                         <DrawerTitle>Crear Detalle</DrawerTitle>
+                        <DrawerDescription>
+                            Crea un nuevo detalle para la factura
+                        </DrawerDescription>
                     </DrawerHeader>
+                    <DrawerFooter>
+                        <Field orientation={'horizontal'}>
+                            <form.AppForm>
+                                <form.FormButton label='Guardar' />
+                            </form.AppForm>
+                            <DrawerClose asChild>
+                                <Button variant='outline'>Cerrar</Button>
+                            </DrawerClose>
+                        </Field>
+                    </DrawerFooter>
                 </form>
             </DrawerContent>
         </Drawer>
